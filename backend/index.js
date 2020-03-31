552612006cd4ac256f721cb973e69b048561846c
Mateus Amaral <mateus_sousa_amaral@hotmail.com>
2020-03-31 17:09:59 -0300

    Initial Commit
    preparando ambientes.

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3333);
+
+
+app.get('/', (request, response) =>{
+    return response.json({
+        evento: 'Semana Omnistack',
+        aluno: 'Mateus'
+    });
+});
+
